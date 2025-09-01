f28b56550caf6aa604c80520a275c2099ce9cbee
Muazam Qayyum <csmuazam@gmail.com>
2025-09-01 12:31:55 +0500

    router practice and project

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/home'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <Footer />
+      <Home />
+
+    </>
+  )
+}
+
+export default App
